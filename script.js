210305eca3d15ddb464a96a52b92e157ff6c176a
herilion <heritierlionge@yahoo.com>
2023-01-09 16:15:29 +0200

    decrementation

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const numb = document.querySelector(".numb");
-let counter = 0;
+let counter = 100;
 setInterval(() => {
-    if (counter == 100) {
+    if (counter == 0) {
         clearInterval();
     } else {
-        counter += 1;
+        counter -= 1;
         numb.textContent = counter + "%";
     }
 }, 80);
